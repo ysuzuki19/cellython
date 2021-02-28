32ecd0ffae207bf5fbfcc86038ce67b071ad354b
ysuzuki19 <ysuzuki.cpp@gmail.com>
2021-02-28 18:16:39 +0900

    [api] initializer

--- api/src/__init__.py
+++ api/src/__init__.py
@@ -1,8 +1,35 @@
-import time
 from flask import Flask
+import json
+from json import JSONEncoder
+import numpy as np
 
 app = Flask(__name__)
 
-@app.route('/api/time')
-def get_current_time():
-  return {'time': time.time()}
+data_path = '../data/data.csv'
+
+matrix = []
+with open(data_path) as f:
+  lines = f.readlines()
+  for line in lines:
+    line = line.strip('\n')
+    line = line.split(',')
+    matrix.append(line)
+matrix = np.array(matrix)
+print(matrix)
+
+class NumpyArrayEncoder(JSONEncoder):
+  def default(self, obj):
+    if isinstance(obj, np.ndarray):
+      return obj.tolist()
+    return JSONEncoder.default(self, obj)
+
+@app.route('/init')
+def get_init_data():
+  encodeNumpyData = json.dumps(
+      {'array': matrix},
+      cls=NumpyArrayEncoder)
+  return encodeNumpyData
+
+@app.route('/calc')
+def get_calc():
+  pass
